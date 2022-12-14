7134047de1d6d67d8020614e7edfd60103842269
snahyy <108914151+snahyy@users.noreply.github.com>
2022-12-13 23:14:36 -0300

    update produto

--- src/main/java/healingPharmacy/model/Produto.java
+++ src/main/java/healingPharmacy/model/Produto.java
@@ -14,6 +14,18 @@ public class Produto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer prod_id;
 
+    @Column
+    private String nome;
+
+    @Column
+    private String marca;
+
+    @Column
+    private String categoria;
+
+    @Column
+    private Integer quantidade;
+
     @Column(nullable = false,length = 150)
     private String descricao;
 }
