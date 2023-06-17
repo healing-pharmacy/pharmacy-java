a967c7d3794bc1d4cd1690fee7f5c0cb7eac5918
samuelcostta <samuelcostta2306@gmail.com>
2023-06-17 15:57:35 -0300

    Implementação do put e delete

--- src/main/java/healingPharmacy/rest/PacienteController.java
+++ src/main/java/healingPharmacy/rest/PacienteController.java
@@ -4,6 +4,7 @@ import healingPharmacy.model.Paciente;
 import healingPharmacy.repository.IPaciente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -20,40 +21,58 @@ public class PacienteController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Paciente salvar(@RequestBody Paciente paciente){
-        System.out.println(paciente.getNome_paciente());
+        //System.out.println(paciente.getNome_paciente());
         return dao.save(paciente);
-        //return pacienteNovo;
     }
 
-    /*@GetMapping("{id}")
-    public Paciente acharPorId (@PathVariable Integer id ){
-        return dao
-                .findById(id)
-                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-    }*/
+    @GetMapping("/{id_paciente}")
+    public ResponseEntity<Paciente> acharPorId (@PathVariable Integer id_paciente ){
+        return dao.findById(id_paciente)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
 
-    /*@DeleteMapping("{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletar (@PathVariable Integer id){
-        dao
-                .findById(id)
-                .map(paciente -> {
-                    dao.delete(paciente);
-                    return Void.TYPE;                })
-                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Paciente não encontrado"));
+                //.findById(id)
+                //.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 
-    }*/
+    @PutMapping("/{id_paciente}")
+    public ResponseEntity<Paciente> atualizar (@PathVariable Integer id_paciente, @RequestBody Paciente paciente){
+        return dao.findById(id_paciente)
+                .map(recordFound -> {
+                    recordFound.setNome_paciente(paciente.getNome_paciente());
+                    recordFound.setData_nascimento(paciente.getData_nascimento());
+                    recordFound.setEmail(paciente.getEmail());
+                    recordFound.setCpf(paciente.getCpf());
+                    recordFound.setEndereco(paciente.getEndereco());
+                    recordFound.setTelefone(paciente.getTelefone());
+                    Paciente atualizado = dao.save(recordFound);
+                    return ResponseEntity.ok().body(atualizado);
+                })
+                .orElse(ResponseEntity.notFound().build());
 
-    /*@PutMapping("{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void atualizar (@PathVariable Integer id, @RequestBody @Valid Paciente pacienteAtualizado){
-        dao
-                .findById(id)
+                /*.findById(id_paciente)
                 .map(paciente -> {
                     pacienteAtualizado.setId_paciente(paciente.getId_paciente());
                     return dao.save(paciente);                })
-                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Paciente não encontrado"));
-    }*/
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Paciente não encontrado"));*/
+    }
+
+    @DeleteMapping("/{id_paciente}")
+    public ResponseEntity<Void> deletar (@PathVariable Integer id_paciente){
+        return dao.findById(id_paciente)
+                .map(recordFound -> {
+                    dao.deleteById(id_paciente);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+
+                /*.findById(id_paciente)
+                .map(paciente -> {
+                    dao.delete(paciente);
+                    return Void.TYPE;                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Paciente não encontrado"));*/
+
+    }
 
     @GetMapping()
     public List<Paciente> list(){
